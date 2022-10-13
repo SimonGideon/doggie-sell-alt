181747187f959f22962b844879105f4d6cc52bf0
SimonGideon <simongideon918@gmail.com>
2022-10-14 01:39:55 +0300

    Solve linters

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const menuBtn = document.querySelector('.menu-btn');
 const hamburger =document.querySelector('.menu-btn_burger');
 let showMenu = false;
 const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('menu-nav');
+const menuNav = document.querySelector('.menu-nav');
 
 menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu(){
     if(!showMenu){
         hamburger.classList.add('open');
         nav.classList.add('open');
-        menuNav.classList,add('open');
+        menuNav.classList.add('open');
         showMenu = true;
     } else {
         hamburger.classList.remove('open');
