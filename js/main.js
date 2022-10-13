2a104b60d8c6b12c028fb1d77da37c1cc994135d
SimonGideon <simongideon918@gmail.com>
2022-10-14 00:43:19 +0300

    Menu-items

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger =document.querySelector('.menu-btn_burger');
+let showMenu = false;
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('menu-nav');
+
+menuBtn.addEventListener('click', toggleMenu);
+function toggleMenu(){
+    if(!showMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList,add('open');
+        showMenu = true;
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        showMenu = false;
+    }
+}
